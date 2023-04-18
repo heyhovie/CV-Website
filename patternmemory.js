5d9083164958ecd8fa10785dea4a8118a0518363
Takunda Hove <takundahove@icloud.com>
2023-04-18 22:07:34 +0200

    Update patternmemory.js

--- patternmemory.js
+++ patternmemory.js
@@ -1,5 +1,7 @@
 function start(){
     document.getElementById('startPanel').style.display = 'none';
+    document.getElementsByClassName('player-screen-content')[0].style.display = 'block';
+    document.getElementById('play').style.display = 'none';
 }
 
 function reset(){
